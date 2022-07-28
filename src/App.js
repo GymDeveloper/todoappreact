f481c148a681c2fcd4656aa1c865925af6fcb012
linder3hs <linderhassinger00@gmail.com>
2022-07-27 20:14:24 -0500

    todoapp

--- src/App.js
+++ src/App.js
@@ -1,42 +1,9 @@
-import { useState, useRef } from "react";
-import FormAddTask from "./components/FormAddTask";
-import TaskListContainer from "./components/TaskListContainer";
-import TodoApi from "./services/todoapi";
+import Task from "./pages/Task";
 
 function App() {
-  const inputTask = useRef(null);
-
-  const [text, setText] = useState("");
-
-  const [tasks, setTasks] = useState([]);
-
-  function manejarInput(e) {
-    setText(e.target.value);
-  }
-
-  async function agregar() {
-    const res = await TodoApi.addtask(text);
-    if (res) {
-      inputTask.current.value = "";
-      inputTask.current.focus();
-      refrescar();
-    }
-  }
-
-  async function refrescar() {
-    const data = await TodoApi.listtasks();
-    setTasks(data.tasks);
-  }
-
   return (
-    <div className="container mt-5 mx-auto" style={{ width: 600 }}>
-      <FormAddTask
-        inputTask={inputTask}
-        agregar={agregar}
-        manejarInput={manejarInput}
-        refrescar={refrescar}
-      />
-      <TaskListContainer tasks={tasks} />
+    <div>
+      <Task />
     </div>
   );
 }
---
+++ src/pages/Task/index.js
@@ -0,0 +1,44 @@
+import { useState, useRef } from "react";
+import FormAddTask from "../../components/FormAddTask";
+import TaskListContainer from "../../components/TaskListContainer";
+import TodoApi from "../../services/todoapi";
+
+const Task = () => {
+  const inputTask = useRef(null);
+
+  const [text, setText] = useState("");
+
+  const [tasks, setTasks] = useState([]);
+
+  function manejarInput(e) {
+    setText(e.target.value);
+  }
+
+  async function agregar() {
+    const res = await TodoApi.addtask(text);
+    if (res) {
+      inputTask.current.value = "";
+      inputTask.current.focus();
+      refrescar();
+    }
+  }
+
+  async function refrescar() {
+    const data = await TodoApi.listtasks();
+    setTasks(data.tasks);
+  }
+
+  return (
+    <div className="container mt-5 mx-auto" style={{ width: 600 }}>
+      <FormAddTask
+        inputTask={inputTask}
+        agregar={agregar}
+        manejarInput={manejarInput}
+        refrescar={refrescar}
+      />
+      <TaskListContainer tasks={tasks} />
+    </div>
+  );
+};
+
+export default Task;
