e288eb8ea6dbe1fbb7094a10c9ca26452e1e9046
linder3hs <linderhassinger00@gmail.com>
2022-07-27 21:35:51 -0500

    todoapp

--- src/components/TaskItem/index.js
+++ src/components/TaskItem/index.js
@@ -1,10 +1,28 @@
+import Swal from "sweetalert2";
+import TodoApi from "../../services/todoapi";
+
 const TaskItem = ({ index, task }) => {
+  const checkTask = async () => {
+    const response = await Swal.fire({
+      title: `Estas seguro de marcar como terminado la tarea ${task.text}?`,
+      showDenyButton: true,
+      confirmButtonText: "Si",
+      denyButtonText: "No",
+    });
+
+    // si response.value es true
+    if (response.value) {
+      // entonces llamamos al servicio doneTask y le pasamos el id
+      await TodoApi.doneTask(task.id);
+    }
+  };
+
   return (
     <li key={index} className="list-group-item">
       <div className="row">
         <div className="col-md-8">{task.text}</div>
         <div className="col-md-4">
-          <button className="btn btn-sm btn-success">
+          <button onClick={checkTask} className="btn btn-sm btn-success">
             <i className="fa fa-check"></i>
           </button>
           &nbsp;
